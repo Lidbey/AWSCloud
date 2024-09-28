aeedb27cae7328eece1666a473120b29337ae090
wicki <wojciech@ninjageeks.pl>
2024-09-28 13:54:11 +0200

    Added functions to upload, download and search list of files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import boto3
+import re
 import credentials
 import os
 
@@ -15,27 +16,61 @@ s3 = boto3.resource(
 
 x_wing_bucket = s3.Bucket(name='developer-task')
 
-files = [bucket.key for bucket in x_wing_bucket.objects.filter(Prefix='x-wing')]
+files = [str(bucket.key) for bucket in x_wing_bucket.objects.filter(Prefix='x-wing')]
 
 
 def listFiles(bucket, body):
     if body == '':
         print(files)
         return
+    if len(body) < 2:
+        print('No regex provided')
+        return
+    regex = body[1:]
+    print(regex)
+    r = re.compile(regex)
+    print(list(filter(r.match, files)))
 
 
 def uploadFile(bucket, body):
     paths = body.split('\'')[1::2]
 
+    if len(paths) < 1:
+        print('No file provided')
+        return
+
+    file_path = paths[0]
+    file_name = BUCKET_PREFIX + '/' + (os.path.basename(file_path) if len(paths) < 2 else paths[1])
+    try:
+        response = bucket.upload_file(file_path, file_name)
+        print(f'File uploaded. Response {response}')
+        if file_name not in files:
+            files.append(file_name)
+    except Exception as e:
+        print(f'Failed to upload with error {e}')
+
 
 def downloadFile(bucket, body):
     paths = body.split('\'')[1::2]
 
+    if len(paths) < 2:
+        print('Paths not provided')
+        return
+
+    remote_path = paths[0] if paths[0].startswith(BUCKET_PREFIX+'/') else BUCKET_PREFIX + '/' + paths[0]
+    local_path = paths[1]
+
+    try:
+        response = bucket.download_file(remote_path, local_path)
+        print(f'File downloaded. Response {response}')
+    except Exception as e:
+        print(f'Failed to download with error {e}')
 
 def deleteFile(bucket, body):
     if body == '':
         print('No regex provided')
         return
+    regex = body.split('\'')[1::2]
 
 
 print(f"""
@@ -43,8 +78,8 @@ Welcome to AWS CLI for buckets.
 You're connected to bucket {BUCKET_NAME}. Your searching path is {BUCKET_PREFIX}
 Available commands:
     lst [regex]- lists all files matching regex 
-    upl '[local_file_path]' '[remote_name]' - uploads file named file_path to the bucket
-    dwn '[remote_file_path]' '[local_file_path]' - downloads file named file_path to PATH
+    upl '[local_file_path]' '[remote_name]' - uploads file to the bucket
+    dwn '[remote_file_path]' '[local_file_path]' - downloads file from the bucket
     del [regex] - deletes file matching regex
     cls - quits the application
 """)
